f212e23d94c1ec875937364861d3193ae08c7e80
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-19 16:59:20 -0300

    Correcting Program (toString)

--- 0x00/personagem/Personagem.java
+++ 0x00/personagem/Personagem.java
@@ -55,14 +55,14 @@ public abstract class Personagem {
     @Override
     public String toString() {
         return "Personagem{" +
-                "nome='" + nome + '\'' +
-                ", tipo=" + tipo +
-                ", inteligencia=" + inteligencia +
-                ", forca=" + forca +
-                ", vigor=" + vigor +
-                ", resistencia=" + resistencia +
-                ", destreza=" + destreza +
-                ", dano ataque =" + String.format("%.2f", getDanoAtaque()) +
+                "nome = '" + nome + '\'' +
+                ", tipo = " + tipo +
+                ", inteligencia = " + inteligencia +
+                ", forca = " + forca +
+                ", vigor = " + vigor +
+                ", resistencia = " + resistencia +
+                ", destreza = " + destreza +
+                ", dano ataque = " + String.format("%.2f", getDanoAtaque()) +
                 '}';
     }
 }
