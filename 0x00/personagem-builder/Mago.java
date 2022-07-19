6ad0a47acfd4d0802311105e2cde21d9a8debd63
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-19 17:34:19 -0300

    Add files via upload

--- 0x00/personagem-builder/Mago.java
+++ 0x00/personagem-builder/Mago.java
@@ -12,6 +12,6 @@ public class Mago extends Personagem {
 
     @Override
     public double getDanoAtaque() {
-        return this.getInteligencia() * 0.8 + this.getForca() * 0.5 + this.getDestreza() * 0.05 + this.getVigor() * 0.05;
+        return this.getInteligencia() * 0.8 + this.getForca() * 0.05 + this.getDestreza() * 0.05 + this.getVigor() * 0.1;
     }
 }
