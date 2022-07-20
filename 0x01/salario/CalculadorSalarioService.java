49c900bbefd3dc74e6cda8031e5e28666d7f4bdc
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-20 15:13:16 -0300

    Correcting path to checker

---
+++ 0x01/salario/CalculadorSalarioService.java
@@ -0,0 +1,32 @@
+import java.util.UUID;
+
+public class CalculadorSalarioService {
+
+    private static CalculadorSalarioService instancia;
+    public UUID uuid = UUID.randomUUID();
+
+    private CalculadorSalarioService() {
+    }
+
+    public static CalculadorSalarioService getInstancia() {
+        return instancia;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public static CalculadorSalarioService getCalculadorSalarioService(){
+        if (instancia == null){
+            return new CalculadorSalarioService();
+        }
+        return instancia;
+    }
+
+
+
+    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
+        return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
+    }
+
+}
