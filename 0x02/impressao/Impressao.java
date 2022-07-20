271a91737f978978043fd6993ceaffb3a657c798
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-20 17:23:08 -0300

    Correcting path

---
+++ 0x02/impressao/Impressao.java
@@ -0,0 +1,38 @@
+public class Impressao {
+
+    private int paginasTotais, paginasColoridas;
+    private boolean ehFrenteVerso;
+    private double valorColoridasFrenteVerso, valorPretoBrancoFrenteVerso, valorColoridasFrenteApenas, valorPretoBrancoFrenteApenas;
+
+    public Impressao(int paginasTotais, int paginasColoridas, boolean ehFrenteVerso, double valorColoridasFrenteVerso, double valorPretoBrancoFrenteVerso, double valorColoridasFrenteApenas, double valorPretoBrancoFrenteApenas) {
+        this.paginasTotais = paginasTotais;
+        this.paginasColoridas = paginasColoridas;
+        this.ehFrenteVerso = ehFrenteVerso;
+        this.valorColoridasFrenteVerso = valorColoridasFrenteVerso;
+        this.valorPretoBrancoFrenteVerso = valorPretoBrancoFrenteVerso;
+        this.valorColoridasFrenteApenas = valorColoridasFrenteApenas;
+        this.valorPretoBrancoFrenteApenas = valorPretoBrancoFrenteApenas;
+    }
+
+    public double calcularTotal() {
+        if (ehFrenteVerso) {
+            return paginasColoridas * valorColoridasFrenteVerso + (paginasTotais - paginasColoridas) * valorPretoBrancoFrenteVerso;
+        }
+
+        return paginasColoridas * valorColoridasFrenteApenas + (paginasTotais - paginasColoridas) * valorPretoBrancoFrenteApenas;
+    }
+
+    @Override
+    public String toString() {
+
+        String frenteVerso = ehFrenteVerso ? "frente e verso" : "frente apenas";
+
+        return "total de paginas: " + paginasTotais +
+                ", total coloridas: " + paginasColoridas +
+                ", total preto e branco: " + (paginasTotais - paginasColoridas) +
+                ", " + frenteVerso +
+                ". total: R$ " + String.format("%.2f", calcularTotal());
+    }
+
+
+}
