f275f787e0a4e6345c57d6a85f79a7c1350bdd5d
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-21 10:12:04 -0300

    Correcting path on checker

---
+++ 0x03/midia_player/AudioPlayer.java
@@ -0,0 +1,10 @@
+public class AudioPlayer implements MediaPlayer{
+    @Override
+    public void reproduzir(TipoMedia tipoMedia, String nome) {
+        if (tipoMedia.equals(TipoMedia.MP3)){
+            System.out.println("Reproduzindo MP3: " + nome);
+        }
+
+         new MediaPlayerAdapter(tipoMedia).reproduzir(tipoMedia, nome);
+    }
+}
