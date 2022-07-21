206b88e6dce18f443f97376996d1984d460fa55e
Le Leonel <105403149+leleonelciandt@users.noreply.github.com>
2022-07-21 10:14:35 -0300

    Correcting path

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -2,7 +2,7 @@ public class AudioPlayer implements MediaPlayer{
     @Override
     public void reproduzir(TipoMedia tipoMedia, String nome) {
         if (tipoMedia.equals(TipoMedia.MP3)){
-            System.out.println("Reproduzindo MP3: " + nome);
+            System.out.print("Reproduzindo MP3: " + nome);
         }
 
          new MediaPlayerAdapter(tipoMedia).reproduzir(tipoMedia, nome);
